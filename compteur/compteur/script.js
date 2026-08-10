a081abfcc743425265c3acd3fba3999eb4399637
stax_phenix <soench321@gmail.com>
2026-08-10 03:26:11 +0200

    save

--- compteur/compteur/script.js
+++ compteur/compteur/script.js
@@ -3,10 +3,17 @@ const minus = document.getElementById("minus")
 const plus = document.getElementById("plus")
 const reset = document.getElementById("reset")
 let countnumber = Number(count.textContent)
+const message = document.getElementById("message")
+
+
 plus.addEventListener("click", function() {
     if (countnumber < 10) {
         countnumber = countnumber + 1
         count.textContent = countnumber
+        count.style.color = "white"
+        if (countnumber === 10) {
+            count.textContent = "🔥 10 🔥"
+            count.style.color = "red" }
     }
 })
 
@@ -14,12 +21,15 @@ minus.addEventListener("click", function() {
     if (countnumber > 0) {
         countnumber = countnumber -1
         count.textContent = countnumber
+        count.style.color = "white"   
     }
-
+    else { message.textContent = "impossibe de descendre plus bas" 
+        setTimeout(function() { message.textContent = ""   
+}, 2000) }
 })
 
 reset.addEventListener("click", function() {
     countnumber = 0
     count.textContent = countnumber
+    count.style.color = "white"
 })
-console.log("pc gamer")
